47f96d34748fa57143d0bb033e1fbaa5ac15318e
Volodimir Martuniyk <volodimir.martuniyk@gmail.com>
2018-11-02 21:35:55 +0200

    Create Category.index page

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,52 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+<div class="container">
+  @component('admin.components.breadcrumb')
+    @slot('title') Список категорій @endslot
+    @slot('parent') Головна @endslot
+    @slot('active') Категорії @endslot
+  @endcomponent
+  <hr>
+
+  <a href="{{route('admin.category.create')}}" class="btn btn-primary pull-right"><i class="fa fa-plus-square-o"></i>
+    Створити категорію
+  </a>
+  <table class="table table-striped">
+    <thead>
+      <th>Назва</th>
+      <th>Публікація</th>
+      <th сlass="text-right">Дія</th>
+    </thead>
+    <tbody>
+      @forelse( $categories as $category)
+      <tr>
+        <td>{{$category->title}}</td>
+        <td>{{$category->published}}</td>
+        <td>
+          <a href="{{'admin.category.edit', ['id' =>$category->id]}}"><i class="fa fa-edit"></i></a>
+        </td>
+      </tr>
+      @empty
+      <tr>
+        <td colspan="3" class="text-center">
+          <h2>Дані відсутні</h2>
+        </td>
+      </tr>
+      @endforelse
+    </tbody>
+    <tfoot>
+      <tr>
+        <td colspan="3">
+          <ul class="pagination pull-right">
+            {{$categories->links()}}
+
+          </ul>
+        </td>
+      </tr>
+    </tfoot>
+
+  </table>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 */
 Route::group(['prefix' =>'admin','namespace' => 'Admin', 'middleware' =>['auth']], function(){
   Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+  Route::resource('/category','CategoryController', ['as'=>'admin']);
 });
 
 Route::get('/', function () {
